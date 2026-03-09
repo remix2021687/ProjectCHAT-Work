8d87f2ed9eca799814eae205ff7a8ce11262fd69
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2026-03-09 17:13:57 +0100

    Add GASP animation select SingIn and SingUp (Beta)

--- client/src/components/Main/AuthPage/AuthPage.tsx
+++ client/src/components/Main/AuthPage/AuthPage.tsx
@@ -1,10 +1,52 @@
-import { useEffect } from "react"
+import { useState, useRef, useEffect } from "react"
+import { gsap } from "gsap"
+import { Flip } from 'gsap/Flip'
 import icon from "@assets/svg/Overlay+Border.svg"
 
+gsap.registerPlugin(Flip)
+
 export const AuthPage: React.FC = () => {
+    const tabsData: Array<{id: string; title: string}> = [
+        {id:"singUp", title: "Sing Up"},
+        {id:"singIn", title: "Sing In"}
+    ]
+    const [activeTab, setActiveTab] = useState(tabsData[0].id)
+    const indecatorRef = useRef<HTMLSpanElement>(null)
+    const tabsRef = useRef<(HTMLButtonElement | null)[]>([])
+
     useEffect(() => {
         document.title = "Paradox - Auth"
     })
+
+    useEffect(() => {
+        const active = tabsRef.current.find(tab => tab?.dataset.id === activeTab)
+
+        if (active && indecatorRef.current) {
+            indecatorRef.current.style.width = `${active.offsetWidth}px`
+            indecatorRef.current.style.left = `${active.offsetLeft}px`
+        }
+    }, [])
+
+    const handleTabClick = (id: string) => { 
+        if (id === activeTab) return
+
+        const state = Flip.getState(indecatorRef.current)
+        setActiveTab(id);
+
+        const newActive = tabsRef.current.find(tab => tab?.dataset.id === id)
+        if (newActive && indecatorRef.current) {
+            indecatorRef.current.style.left = `${newActive.offsetLeft}px`;
+            indecatorRef.current.style.width = `${newActive.offsetWidth}px`;
+        }
+
+        Flip.from(state, {
+            duration: 0.5,
+            ease: "power2.out",
+            absolute: true
+        })
+
+    }
+
     return (
         <section className="AuthPage">
             <section className="AuthPage_left_block">
@@ -19,7 +61,28 @@ export const AuthPage: React.FC = () => {
                 </p>
             </section>
             <section className="AuthPage_right_block">
-
+                <section className="AuthPage_right_block_select">
+                    {
+                        tabsData.map((tab, index) => (
+                            <button
+                                key={tab.id}
+                                ref={el => { tabsRef.current[index] = el }}
+                                data-id={tab.id}
+                                className={activeTab === tab.id ? "active" : ""}
+                                onClick={() => handleTabClick(tab.id)}
+                            >
+                                <span>{tab.title}</span>
+                            </button>
+                        ))
+                    }
+                    <span ref={indecatorRef} className="AuthPage_right_block_select_indecator"></span>
+                </section>
+                <form className="AuthPage_right_block_form">
+                    <section className="AuthPage_right_block_form_header">
+                        <h2>Welcome back</h2>
+                        <p>Join our community today and start streaming.</p>
+                    </section>
+                </form>
             </section>
         </section>
     )
