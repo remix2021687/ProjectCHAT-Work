ef773b2e74a02f25338f93eb9b3dde0dd4b62148
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2026-03-10 09:53:22 +0100

    Update RegisterForm and styles
    Add all inputs required message and add validation for confirmPassword.
    Update styles span, label and icon

--- client/src/components/Main/AuthPage/components/RegisterForm/RegisterForm.tsx
+++ client/src/components/Main/AuthPage/components/RegisterForm/RegisterForm.tsx
@@ -10,7 +10,7 @@ type FormData = {
 
 
 export const RegisterForm: React.FC = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm<FormData>()
+    const { register, watch, handleSubmit, formState: { errors } } = useForm<FormData>()
 
     const onSubmit = (data: FormData) => {
         console.log(data)
@@ -26,27 +26,48 @@ export const RegisterForm: React.FC = () => {
                 <label htmlFor="username">
                     <h3>Username</h3>
                     <UserIcon className="icon" size={25} />
-                    <input id="username" type="text" placeholder="Username" {...register('username', { required: true })} />
-                    {errors.username && <span className="error">Username is required</span>}
+                    <input id="username" type="text" placeholder="Username" {...register('username', {
+                        required: {
+                            value: true,
+                            message: 'Username is required'
+                    } })} />
                 </label>
+                {errors.username && <span className="error">{errors.username.message}</span>}
+                
                 <label htmlFor="email">
                     <h3>Email</h3>
                     <EnvelopeIcon className="icon" size={25} />
-                    <input id="email" type="email" placeholder="Email" {...register('email', { required: true })} />
-                    {errors.email && <span className="error">Email is required</span>}
+                    <input id="email" type="email" placeholder="Email" {...register('email', {
+                        required: {
+                            value: true,
+                            message: 'Email is required'
+                    } })} />
                 </label>
+                {errors.email && <span className="error">{errors.email.message}</span>}
+                
                 <label htmlFor="password">
                     <h3>Password</h3>
                     <PasswordIcon className="icon" size={25} />
-                    <input id="password" type="password" placeholder="Password" {...register('password', { required: true })} />
-                    {errors.password && <span className="error">Password is required</span>}
+                    <input id="password" type="password" placeholder="Password" {...register('password', {
+                        required: {
+                            value: true,
+                            message: 'Password is required'
+                    } })} />
                 </label>
+                {errors.password && <span className="error">{errors.password.message}</span>}
+
                 <label htmlFor="confirmPassword">
                     <h3>Confirm Password</h3>
                     <PasswordIcon className="icon" size={25} />
-                    <input id="confirmPassword" type="password" placeholder="Confirm Password" {...register('confirmPassword', { required: true })} />
-                    {errors.confirmPassword && <span className="error">Confirm Password is required</span>}
+                    <input id="confirmPassword" type="password" placeholder="Confirm Password" {...register('confirmPassword', {
+                        required: {
+                            value: true,
+                            message: 'Confirm Password is required'
+                        },
+                        validate: (value) => value === watch('password') || 'Passwords do not match'
+                    })} />
                 </label>
+                {errors.confirmPassword && <span className="error">{ errors.confirmPassword.message }</span>}
             </section>
             <button type="submit">Sing Up</button>
         </form>
