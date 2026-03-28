73e133d8ad956df65fed8b75d7a2aab30263c701
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2026-03-28 18:42:08 +0100

    Update CommentBox.tsx

--- client/src/feacher/CommentBox/CommentBox.tsx
+++ client/src/feacher/CommentBox/CommentBox.tsx
@@ -1,3 +1,26 @@
+import avatar from "@assets/img/avatar.png";
+
 export const CommentBox: React.FC = () => {
-	return <section className="CommentBox"></section>;
+	return (
+		<section className="CommentBox">
+			<img
+				src={avatar}
+				alt="avatar"
+			/>
+			<section className="CommentBox_content">
+				<section className="CommentBox_content_header">
+					<h1>
+						Alex Rivers <span>5h ago</span>
+					</h1>
+				</section>
+				<section>
+					<p>
+						This breakdown is exactly what I needed for my current
+						project. The section on spatial layouts was particularly
+						eye-opening!
+					</p>
+				</section>
+			</section>
+		</section>
+	);
 };
